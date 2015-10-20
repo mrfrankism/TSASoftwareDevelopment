2a8ee18684d1ba65bbf9d615a6f329026d7ee103
Hannah <waxhann@aol.com>
2015-10-20 18:28:23 -0400

    Added a new class to hold the different courses offered, and solved different errors.

--- populator/src/pop.java
+++ populator/src/pop.java
@@ -17,8 +17,13 @@ public class pop {
 		Class.forName("com.mysql.jdbc.Driver");
 	      // Setup the connection with the DB
 	      
-
-	      
+		Courses [] classes = new Courses[20];
+		
+		String [] mclasses = {"Pre-calc", "Ap-Calc", "Calc Hrs", "Algebra 2", "Geometry"};
+		String [] eclasses = {"Ap-Lit", "Ap-Lang", "Lit Hrs", "Lang Hrs", "World Literature"};
+		String [] sclasses = {"Chemistry", "Bio", "Physics", "Physical Science", "DE Geology"};
+	    String [] ssclasses = {"US History", "World History", "Human Geo", "Gov", "Econ"};
+	    
 	    int size = 500;	//number of students total
 		int[] ID = new int[size+1];
 		String[] name = new String[size];
@@ -53,15 +58,22 @@ public class pop {
 			}
 			
 			
-			for(int i=1; i<6; i++){	//get count for each subject and their levels (20 total)
-				getNumberOfClasses("math", ("Math "+ i));
-				getNumberOfClasses("social", ("Social "+ i));
-				getNumberOfClasses("science", ("Science "+ i));
-				getNumberOfClasses("english", ("English "+ i));
+			for(int i=0; i<20; i++){	//get count for each subject and their levels (20 total)
+				classes[i] = new Courses("math", mclasses[i/5]);
+				classes[i+1] = new Courses("science", sclasses[i/5]);
+				classes[i+2] = new Courses("english", eclasses[i/5]);
+				classes[i+3] = new Courses("social", ssclasses[i/5]);
+				i=i+3;
+				
 			}
-	
+			for(int i=0; i<20; i++){	//get count for each subject and their levels (20 total)
+				classes[i].setNumberOfStudents(getNumberOfStudents(classes[i].getSubject(), classes[i].getCourseName()));
+				System.out.println("Number of Units for class: "+ classes[i].getUnits());
+				
+				//getNumberOfStudents("math", "Math 1");
+			}
 	}
-	public static int getNumberOfClasses(String subject, String course){
+	public static int getNumberOfStudents(String subject, String course){
 		String query;
 		java.sql.PreparedStatement preparedStmt;
 		ResultSet rs1;
@@ -72,8 +84,8 @@ public class pop {
 			preparedStmt.execute();
 			rs1 = preparedStmt.getResultSet();
 			rs1.absolute(1);
-			System.out.println("Number of classes for " + course + " : "+rs1.getInt("COUNT(" + subject + ")"));	
-			int a= rs1.getInt("COUNT(" + subject + ")");
+			System.out.println("Number of students for " + course + " : "+rs1.getInt(1));	
+			int a= rs1.getInt(1);
 			return a;
 		} catch (Exception e) {
 			System.out.println(e);
